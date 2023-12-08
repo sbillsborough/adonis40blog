005b27d0065fd8e4b46dc035cc62c95d717e6ad1
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-12-08 17:36:18 +0000

    passed data into the index view with a second parameter inside of the view.render of the index method. The second paramter is an object with the property of title. Then passed that property into index.edge with {{ title }}

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -2,8 +2,12 @@
 
 class PostController {
   async index({ view }) {
-    return view.render("posts.index");
+    return view.render("posts.index", {
+      title: "Latest Posts",
+    });
   }
 }
 
 module.exports = PostController;
+
+// if a view is inside of a folder then use the dot notation to access it eg. "posts.index"
