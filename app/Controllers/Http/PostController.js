f9c7ab3d26fcf2e8f2b66997e0804573abae3340
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-12-12 16:06:57 +0000

    added some hard coded data to loop through and render on the page

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -2,8 +2,15 @@
 
 class PostController {
   async index({ view }) {
+    const posts = [
+      { title: "Post One", body: "This is post one" },
+      { title: "Post Two", body: "This is post two" },
+      { title: "Post Three", body: "This is post three" },
+    ];
+
     return view.render("posts.index", {
       title: "Latest Posts",
+      posts: posts,
     });
   }
 }
