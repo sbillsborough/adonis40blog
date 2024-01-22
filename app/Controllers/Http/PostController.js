d7ab9b741a6b5cee8cb4aed23001301267f1047c
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2024-01-22 16:48:20 +0000

    commented out the hard coded data and await Post model call with find all() method. Added toJSON() to posts array

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -5,15 +5,17 @@ const Post = use("App/Models/Post");
 
 class PostController {
   async index({ view }) {
-    const posts = [
-      { title: "Post One", body: "This is post one" },
-      { title: "Post Two", body: "This is post two" },
-      { title: "Post Three", body: "This is post three" },
-    ];
+    // const posts = [
+    //   { title: "Post One", body: "This is post one" },
+    //   { title: "Post Two", body: "This is post two" },
+    //   { title: "Post Three", body: "This is post three" },
+    // ];
+
+    const posts = await Post.all();
 
     return view.render("posts.index", {
       title: "Latest Posts",
-      posts: posts,
+      posts: posts.toJSON(),
     });
   }
 }
